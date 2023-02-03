49869cff74428241596fb056bd38d1f3de7b2ea5
Herbert Vieira - CODER'S <84937026+Betinhovieira@users.noreply.github.com>
2023-02-03 15:46:43 -0300

    Alteração dos codigos de footer e menu

--- app.js
+++ app.js
@@ -42,3 +42,34 @@ function setAria() {
 
 btnMenu.addEventListener("click", handleButtonClick);
 btnMenu.addEventListener("touchstart", handleButtonClick);
+
+const changeThemeBtn = document.querySelector("#change-theme")
+
+    // Toggle dark mode
+    function toggleDarkmode() {
+    document.body.classList.toggle("dark");
+    }
+
+    // Load ligth or dar mode
+    function loadTheme(){
+        const darkMode = localStorage.getItem("dark")
+
+        if(darkMode){
+            toggleDarkmode();
+
+        }
+    }
+
+    loadTheme();
+
+    // Load ligth or dark mode
+    changeThemeBtn.addEventListener("change", function() {
+    document.body.classList.toggle("dark");
+
+    // save or remove dark mode
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark", 1);
+    }
+})
